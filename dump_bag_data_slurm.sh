16097109b889d84ada27755daf633205af13faa9
Kyle Vedder <kyle.c.vedder@gmail.com>
2021-10-23 19:07:53 -0400

    Fixed container and several syntax errors

--- dump_bag_data_slurm.sh
+++ dump_bag_data_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-srun 
+srun \
  --time=00:40:00\
  --container-mounts=/scratch:/scratch,/home/kvedder/code/bag_processing:/project,/Datasets:/Datasets\
  --container-image=ros:melodic-ros-base-bionic \
-bash -c "./dump_bag_data.py2 /scratch/kvedder/ros_raw_data/ ./dump_bag_data.py2 data/data_dump_raw/ /scratch/kvedder/_2021-10-15*.bag"
+bash -c "cd /project; ./dump_bag_data.py2 /scratch/kvedder/ros_raw_data/ ./dump_bag_data.py2 data/data_dump_raw/ /scratch/kvedder/_2021-10-15*.bag"
